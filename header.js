b077de5ff4ebd8898b3cc3ae3a30f767f5631093
Abhinav Saraswat <67242771+abhinavsaraswatt@users.noreply.github.com>
2022-01-29 16:54:45 +0530

    Perfectly completed slider feature in header section

---
+++ header.js
@@ -0,0 +1,36 @@
+"use strict";
+
+const header = document.querySelector("header");
+
+// scrollLeftMax => only valid in firefox
+// (header.scrollWidth - header.clientWidth) valid in all browsers
+
+header.scrollTo((header.scrollWidth - header.clientWidth) / 2, 0);
+
+// SLIDER
+const slider = document.querySelector("header");
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener("mousedown", (e) => {
+  isDown = true;
+  slider.classList.add("active");
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+slider.addEventListener("mouseleave", () => {
+  isDown = false;
+  slider.classList.remove("active");
+});
+slider.addEventListener("mouseup", () => {
+  isDown = false;
+  slider.classList.remove("active");
+});
+slider.addEventListener("mousemove", (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 3; //scroll-fast
+  slider.scrollLeft = scrollLeft - walk;
+});
